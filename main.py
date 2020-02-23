760b83269f34a393636d4d06749034c1763b288f
brian-macomber <52986821+brian-macomber@users.noreply.github.com>
2020-02-22 19:31:10 -0500

    have tweets from last 24 hours in a list

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import tweepy
+from datetime import datetime, timedelta
 from secret import consumer_key
 from secret import consumer_key_secret
 from secret import access_token
 from secret import access_token_secret
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
-auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth, wait_on_rate_limit=True)
+def get_Tweets(screen_name):
+    # handling authentiation using tweepy
+    auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-timeline = api.user_timeline(screen_name="Brian_Macomber", count=5)
+    # actually creates interface to call twitter api
+    api = tweepy.API(auth, wait_on_rate_limit=True)
 
-for tweet in timeline:
-    print(tweet.text.encode("utf-8"))
+    timeline = api.user_timeline(screen_name=screen_name, count=100,\
+         exclude_replies=True, include_rts=1)
+
+    today = datetime.now()
+
+    currTweets = []
+
+    for tweet in timeline:
+        # check if tweet was within the last 24 hours
+        if today-timedelta(hours=24) <= tweet.created_at <= today:
+            currTweets.append(tweet.text)
+    return currTweets
+
+
+def tweet_2_image(tweet_list):
+    for tweet in tweet_list:
+        print(type(tweet))
+
+
+
+
+if __name__ == "__main__":
+    tweets = get_Tweets("overwatchleague")
+    yonk = tweet_2_image(tweets)
